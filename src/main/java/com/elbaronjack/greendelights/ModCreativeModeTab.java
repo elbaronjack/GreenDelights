cead686716caa87bb30963e1b6305e3f178ab91c
Baron Jack <77287606+elbaronjack@users.noreply.github.com>
2025-03-16 13:35:59 +0100

    - Brewin and Chewin 1.20.1 is now compatible with Green Delights 1.20.1

--- src/main/java/com/elbaronjack/greendelights/ModCreativeModeTab.java
+++ src/main/java/com/elbaronjack/greendelights/ModCreativeModeTab.java
@@ -1,6 +1,8 @@
 package com.elbaronjack.greendelights;
 
+import com.elbaronjack.greendelights.common.item.BCItems;
 import com.elbaronjack.greendelights.common.item.ModItems;
+import com.elbaronjack.greendelights.util.CompatUtils;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
@@ -15,6 +17,14 @@ public class ModCreativeModeTab {
             () -> CreativeModeTab.builder()
                     .title(Component.translatable("itemGroup.greendelights"))
                     .icon(() -> new ItemStack(ModItems.SLIME_MOUSSE.get()))
-                    .displayItems((parameters, output) -> ModItems.CREATIVE_TAB_ITEMS.forEach((item) -> output.accept(item.get())))
+                    .displayItems((parameters, output) -> {
+                        // Add main mod items
+                        ModItems.CREATIVE_TAB_ITEMS.forEach((item) -> output.accept(item.get()));
+
+                        // Add Brewin' and Chewin' compatibility items if mod is present
+                        if (CompatUtils.areModsLoaded("brewinandchewin")) {
+                            BCItems.CREATIVE_TAB_ITEMS.forEach((item) -> output.accept(item.get()));
+                        }
+                    })
                     .build());
 }
